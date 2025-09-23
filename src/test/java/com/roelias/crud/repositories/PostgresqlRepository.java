d48aaf7a60b91ccde38cd869293c73aed023d413
elialm7 <metall65eli@gmail.com>
2025-09-23 15:03:17 -0300

    feat: removed support for searcheable queries, too much complexity

--- src/test/java/com/roelias/crud/repositories/PostgresqlRepository.java
+++ src/test/java/com/roelias/crud/repositories/PostgresqlRepository.java
@@ -1,8 +1,12 @@
 package com.roelias.crud.repositories;
 
 import com.roelias.crud.CRUD;
+
+import com.roelias.crud.entities.TestSimpleUserPostgresql;
 import com.roelias.crud.entities.TestUserPostgresql;
 import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
 
 public class PostgresqlRepository extends CRUD<TestUserPostgresql, Long> {
 
@@ -10,4 +14,12 @@ public class PostgresqlRepository extends CRUD<TestUserPostgresql, Long> {
         super(jdbi, TestUserPostgresql.class, Long.class);
     }
 
+    public List<TestSimpleUserPostgresql> listSimpleUsers() {
+        return jdbi.withHandle(handle -> {
+
+            return handle.createQuery("SELECT * FROM " + getTableName())
+                    .map(getCustomRowMapper(TestSimpleUserPostgresql.class))
+                    .list();
+        });
+    }
 }
--- src/test/java/com/roelias/crud/test/TestPostgresql.java
+++ src/test/java/com/roelias/crud/test/TestPostgresql.java
@@ -192,6 +192,7 @@ public class TestPostgresql {
         }
     }
 
+
     @Test
     void testCreationTableTemplate(){
         System.out.println(repository.getTableCreationTemplate());
